4b4c7223d08d829cdc7c2e149cf0bcc9516690ef
Huong123a <ledieuhuong22122001>
2023-10-01 16:39:43 +0700

    push git

--- admin/product-manage.js
+++ admin/product-manage.js
@@ -1,6 +1,5 @@
 const productsDB = JSON.parse(localStorage.getItem("products"));
 function renderManager(data) {
-  
   const listProduct = document.querySelector("#list-prouct");
   let resurl = "";
   console.log(1111, productsDB);
@@ -41,12 +40,41 @@ function handleDeleteManage(ID) {
   renderManager(productsDB);
 }
 
-
-function searchProductEl(){
-  const searchProduct = document.querySelector('#search-product').value.toLowerCase();
-   const products =getDataFromLocalStorage('products');
-  const newProduct = products.filter((product)=>{
-    return product.name.toLowerCase().includes(searchProduct)
+function searchProductEl() {
+  const searchProduct = document
+    .querySelector("#search-product")
+    .value.toLowerCase();
+  const products = getDataFromLocalStorage("products");
+  const newProduct = products.filter((product) => {
+    return product.name.toLowerCase().includes(searchProduct);
   });
-  renderManager(newProduct)
+  renderManager(newProduct);
+}
+function handleAddProduct() {
+  const productsDB = JSON.parse(localStorage.getItem("products"));
+  const idProductElement = document.querySelector("#product-id");
+  const nameProductElement = document.querySelector("#product-name");
+  const skinProductElement = document.querySelector("#product-skin");
+  const priceProductElement = document.querySelector("#product-price");
+  const qantityProductElement = document.querySelector("#product-qantity");
+  const descriptionProductElement = document.querySelector(
+    "#product-description"
+  );
+  const newProduct = {
+    ID: idProductElement.value,
+    name: nameProductElement.value,
+    skin: skinProductElement.value,
+    price: Number(priceProductElement.value),
+    qantity: Number(qantityProductElement.value),
+    description: descriptionProductElement.value,
+  };
+  productsDB.push(newProduct);
+  localStorage.setItem("products", JSON.stringify(productsDB));
+  renderManager(productsDB);
+  idProductElement.value = "";
+  nameProductElement.value = "";
+  skinProductElement.value = "";
+  priceProductElement.value = "";
+  qantityProductElement.value = "";
+  descriptionProductElement.value = "";
 }
